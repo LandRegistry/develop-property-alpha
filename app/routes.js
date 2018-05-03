5cf1be5e53bb58a7c52047697753ff19de9fc6a3
Andy Porter <andy.porter@digital.landregistry.gov.uk>
2018-05-03 18:22:20 +0100

    CAD data? routing

--- app/routes.js
+++ app/routes.js
@@ -8,4 +8,25 @@ router.get('/', function (req, res) {
 
 // add your routes here
 
+
+// /////////////////////////
+// Check Development Plan V1
+// /////////////////////////
+
+// CAD data routing
+
+router.get('/check-my-plans/initial-check/v1/03-upload-cad', function (req, res) {
+  // get the answer from the query string (eg. ?hasCAD=false)
+  var hasCAD = req.query.hasCAD
+
+  if (hasCAD === 'false') {
+    // redirect to the relevant page
+    res.redirect('/check-my-plans/initial-check/v1/03b-no-cad')
+  } else {
+    // if hasCAD is any other value (or is missing) render the page requested
+    res.render('check-my-plans/initial-check/v1/03-upload-cad')
+  }
+})
+
+
 module.exports = router
